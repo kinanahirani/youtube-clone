e6a4b858362aa6027ed138825bc3e5de19113ed4
Kinana Hirani <kinanahtridhyatech@gmaiil.com>
2024-02-28 18:34:37 +0530

    Initial commit

---
+++ src/components/Head.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <img
+          //   onClick={() => toggleMenuHandler()}
+          //   className="h-8 cursor-pointer"
+          style={{ height: 30 }}
+          alt="menu"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
+        />
+        <a href="/">
+          <img
+            style={{ height: 30 }}
+            // className="h-8 mx-2"
+            alt="youtube-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+          />
+        </a>
+      </div>
+      <div>
+        <input placeholder="search" />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+          🔍
+        </button>
+      </div>
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTls3RFQ1C3s-TXEmjOtvdfDeL4Izr5R0puSw&usqp=CAU" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
